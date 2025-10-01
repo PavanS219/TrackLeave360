09d5bf15ce8111f7423b12546bc0496359e43501
Pavan <pavantej2200@gmail.com>
2025-10-01 21:17:43 +0530

    Your commit message

---
+++ force-app/main/default/lwc/myLeaves/myLeaves.js
@@ -0,0 +1,112 @@
+import { LightningElement, wire } from 'lwc';
+import getMyLeaves from '@salesforce/apex/LeaveRequstController.getMyLeaves';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import Id from '@salesforce/user/Id';
+import { refreshApex } from '@salesforce/apex';
+
+const COLUMNS = [
+    { label: 'Request Id', fieldName: 'Name', cellAttributes: { class: { fieldName: 'cellClass' } } },
+    { label: 'From Date', fieldName: 'From_Date__c', cellAttributes: { class: { fieldName: 'cellClass' } } },
+    { label: 'To Date', fieldName: 'To_Date__c', cellAttributes: { class: { fieldName: 'cellClass' } } },
+    { label: 'Reason', fieldName: 'Reason__c', cellAttributes: { class: { fieldName: 'cellClass' } } },
+    { label: 'Status', fieldName: 'Status__c', cellAttributes: { class: { fieldName: 'cellClass' } } },
+    { label: 'Manager Comment', fieldName: 'Manager_Comment__c', cellAttributes: { class: { fieldName: 'cellClass' } } },
+    {
+        type: "button", typeAttributes: {
+            label: 'Edit',
+            name: 'Edit',
+            title: 'Edit',
+            value: 'edit',
+            disabled: { fieldName: 'isEditDisabled' }
+        }, cellAttributes: { class: { fieldName: 'cellClass' } }
+    }
+];
+
+export default class MyLeaves extends LightningElement {
+    columns = COLUMNS;
+
+    myLeaves = [];
+    myLeavesWireResult;
+    showModalPopup = false;
+    objectApiName = 'LeaveRequest__c';
+    recordId = '';
+    currentUserId = Id;
+
+    @wire(getMyLeaves)
+    wiredMyLeaves(result) {
+        this.myLeavesWireResult = result;
+        if (result.data) {
+            this.myLeaves = result.data.map(a => ({
+                ...a,
+                cellClass: a.Status__c == 'Approved' ? 'slds-theme_success' : a.Status__c == 'Rejected' ? 'slds-theme_warning' : '',
+                isEditDisabled: a.Status__c != 'Pending'
+            }));
+        }
+        if (result.error) {
+            console.log('Error occured while fetching my leaves- ', result.error);
+        }
+    }
+
+    get noRecordsFound() {
+        return this.myLeaves.length == 0;
+    }
+
+    newRequestClickHandler() {
+        this.showModalPopup = true;
+        this.recordId = '';
+    }
+
+    popupCloseHandler() {
+        this.showModalPopup = false;
+    }
+
+    rowActionHandler(event) {
+        this.showModalPopup = true;
+        this.recordId = event.detail.row.Id;
+    }
+
+    successHandler(event) {
+        this.showModalPopup = false;
+        this.showToast('Leave request submitted successfully and is pending approval');
+        refreshApex(this.myLeavesWireResult);
+
+        const refreshEvent = new CustomEvent('refreshleaverequests');
+        this.dispatchEvent(refreshEvent);
+    }
+
+    submitHandler(event) {
+        event.preventDefault();
+        const fields = { ...event.detail.fields };
+        fields.Status__c = 'Pending';
+
+        // Normalize dates to midnight
+        let fromDate = new Date(fields.From_Date__c);
+        fromDate.setHours(0, 0, 0, 0);
+
+        let toDate = new Date(fields.To_Date__c);
+        toDate.setHours(0, 0, 0, 0);
+
+        let today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        if (fromDate > toDate) {
+            this.showToast('From date should not be greater than To date', 'Error', 'error');
+        } 
+        else if (fromDate < today) {
+            this.showToast('From date should not be less than Today', 'Error', 'error');
+        } 
+        else {
+            // ✅ Correct way in LWC
+            this.template.querySelector('lightning-record-edit-form').submit(fields);
+        }
+    }
+
+    showToast(message, title = 'Success', variant = 'success') {
+        const event = new ShowToastEvent({
+            title,
+            message,
+            variant
+        });
+        this.dispatchEvent(event);
+    }
+}
